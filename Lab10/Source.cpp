1a9d7795233ec10cb6da729e7a0e762ad973a0cc
SHtabeL <shtabel228@yandex.ru>
2017-02-02 13:31:39 +0300

    Дек перемещён из кастомного класса в STL deque. Файлы кастомного класса удалены за ненадобностью. Код почищен от комментариев.

--- Lab10/Source.cpp
+++ Lab10/Source.cpp
@@ -9,20 +9,21 @@
 #include <cctype>
 #include <wchar.h>
 #include "Phone.h"
-#include "Deque.h"
+#include "deque"
 
 using namespace std;
 
 void wait();
 template <typename TYPE>
-void outputDeque(Structure<TYPE>* structure);
+void outputDeque(deque<TYPE*>* structure);
+void outputDeque(deque<Interface*>* structure);
 
 int main()
 {
 	locale::global(std::locale("Russian"));
 	setlocale(LC_ALL, "Russian");
 	std::wcin.imbue(std::locale("rus_rus.866"));
-	Structure<typename Interface>* interfaceStructure = new Structure<typename Interface>();
+	deque<Interface*>* interfaceStructure = new deque<Interface*>();
 	Interface* interfaceElement;
 	bool dequeType = true; //true: Object; false: standard
 	int command;
@@ -55,77 +56,78 @@ int main()
 		integerTest = new wchar_t[100];
 		switch (command) {
 		case 1:
-				outputDeque(interfaceStructure); //Output case
+			outputDeque(interfaceStructure); //Output case
 			wait();
 			break;
 		case 2: //Push
-				while (true) {
-					system("cls");
-					cout << "Выберите тип элемента, который вы добавите в очередь:" << endl
-						<< "1. Phone" << endl
-						<< "2. ButtonPhone" << endl
-						<< "3. SensorPhone" << endl
-						<< "4. AndroidPhone" << endl << endl
-						<< "Введите команду: ";
-					wcin.getline(integerTest, _msize(integerTest) / sizeof(wchar_t));
+			while (true) {
+				system("cls");
+				cout << "Выберите тип элемента, который вы добавите в очередь:" << endl
+					<< "1. Phone" << endl
+					<< "2. ButtonPhone" << endl
+					<< "3. SensorPhone" << endl
+					<< "4. AndroidPhone" << endl << endl
+					<< "Введите команду: ";
+				wcin.getline(integerTest, _msize(integerTest) / sizeof(wchar_t));
 
-					if (iswdigit(integerTest[0])) { //Проверяем, начинается ли строка с корректного числа
-						command = _wtoi(integerTest); //Переводим число из строки в int
-						if ((command > 0) && (command < 5)) {
-							break;
-						}
-						else {
-							cout << endl << "Неверная команда";
-							wait();
-						}
+				if (iswdigit(integerTest[0])) { //Проверяем, начинается ли строка с корректного числа
+					command = _wtoi(integerTest); //Переводим число из строки в int
+					if ((command > 0) && (command < 5)) {
+						break;
 					}
 					else {
 						cout << endl << "Неверная команда";
 						wait();
 					}
-				switch (command) {
-				case 1:
-					cout << endl << "Объект Phone: " << endl;
-					interfaceElement = new Phone();
-					interfaceElement->input();
-					interfaceStructure->push(interfaceElement);
-					interfaceElement = NULL;
-					break;
-				case 2:
-					cout << endl << "Объект ButtonPhone: " << endl;
-					interfaceElement = new ButtonPhone();
-					interfaceElement->input();
-					interfaceStructure->push(interfaceElement);
-					interfaceElement = NULL;
-					break;
-				case 3:
-					cout << endl << "Объект SensorPhone: " << endl;
-					interfaceElement = new SensorPhone();
-					interfaceElement->input();
-					interfaceStructure->push(interfaceElement);
-					interfaceElement = NULL;
-					break;
-				case 4:
-					cout << endl << "Объект AndroidPhone: " << endl;
-					interfaceElement = new AndroidPhone();
-					interfaceElement->input();
-					interfaceStructure->push(interfaceElement);
-					interfaceElement = NULL;
-					break;
 				}
+				else {
+					cout << endl << "Неверная команда";
+					wait();
+				}
+			}
+			switch (command) {
+			case 1:
+				cout << endl << "Объект Phone: " << endl;
+				interfaceElement = new Phone();
+				interfaceElement->input();
+				interfaceStructure->push_front(interfaceElement);
+				interfaceElement = NULL;
+				break;
+			case 2:
+				cout << endl << "Объект ButtonPhone: " << endl;
+				interfaceElement = new ButtonPhone();
+				interfaceElement->input();
+				interfaceStructure->push_front(interfaceElement);
+				interfaceElement = NULL;
+				break;
+			case 3:
+				cout << endl << "Объект SensorPhone: " << endl;
+				interfaceElement = new SensorPhone();
+				interfaceElement->input();
+				interfaceStructure->push_front(interfaceElement);
+				interfaceElement = NULL;
+				break;
+			case 4:
+				cout << endl << "Объект AndroidPhone: " << endl;
+				interfaceElement = new AndroidPhone();
+				interfaceElement->input();
+				interfaceStructure->push_front(interfaceElement);
+				interfaceElement = NULL;
+				break;
 			}
 			break;
 		case 3: //Pop
-				if (interfaceStructure->getFirst()) {
-					interfaceElement = interfaceStructure->pop();
-					cout << endl << "Элемент был удалён из очереди" << endl;
-					interfaceElement->output();
-					delete interfaceElement;
-				}
-				else {
-					cout << endl << "Очередь пуста, невозможно удалить элемент";
-				}
-				wait();
+			if (interfaceStructure->size() != 0) {
+				interfaceElement = interfaceStructure->back();
+				interfaceStructure->pop_back();
+				cout << endl << "Элемент был удалён из очереди" << endl;
+				interfaceElement->output();
+				delete interfaceElement;
+			}
+			else {
+				cout << endl << "Очередь пуста, невозможно удалить элемент";
+			}
+			wait();
 			break;
 		case 4:
 			cout << endl << "Сериализация структуры" << endl;
@@ -159,23 +161,26 @@ void wait() {
 }
 
 template<typename TYPE>
-void outputDeque(Structure<TYPE>* structure) {
-	Structure<TYPE>::StructureElement* temp = structure->getFirst();
-	if (temp == NULL) {
+void outputDeque(deque<TYPE*>* structure) {
+	if (structure->empty()) {
 		cout << endl << "Очередь пуста";
-		delete temp;
 	}
 	else {
-		for (int i = 1; temp->getElement() != NULL; i++) {
-			cout << endl << "Элемент очереди №" << i << ":" << endl;
-			temp->output();
-			if (temp->getNext()) {
-				temp = temp->getNext();
-			}
-			else {
-				break;
-			}
+		for (unsigned int i = 0; i < structure->size(); i++) {
+			cout << endl << "Элемент очереди №" << (i+1) << ":" << endl;
+			cout << structure->at(i);
 		}
 	}
-	temp = NULL;
+}
+
+void outputDeque(deque<Interface*>* structure) {
+	if (structure->empty()) {
+		cout << endl << "Очередь пуста";
+	}
+	else {
+		for (unsigned int i = 0; i < structure->size(); i++) {
+			cout << endl << "Элемент очереди №" << (i+1) << ":" << endl;
+			structure->at(i)->output();
+		}
+	}
 }
